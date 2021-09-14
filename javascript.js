843618b71b2a5b1d54500c48dd0b291ad5d7fcf7
taylorgooge <taylorgooge@gmail.com>
2021-09-14 17:57:30 -0400

    maybe i'll switch back

--- javascript.js
+++ javascript.js
@@ -1,52 +1,40 @@
-window.onload =  function () {
-    let currentTab = 0; // Current tab is set to be the first tab (0)
-    showTab(currentTab); // Display the current tab
-    
-    function nextPrev(n) {
-        // This function will figure out which tab to display
-        var x = document.querySelectorAll('.tab')
-        console.log(currentTab)
+$(document).ready(function(){
 
-        // Hide the current tab:
-        x[currentTab].style.display = "none";
-        // Increase or decrease the current tab by 1:
-        currentTab = currentTab + n;
-        // if you have reached the end of the form... :
-        if (currentTab >= x.length) {
-            //...the form gets submitted:
-            document.getElementById("msform").submit();
-            return false;
-        }
-        // Otherwise, display the correct tab:
-        showTab(currentTab);
-        }
-
-    function showTab(n) {
-    // This function will display the specified tab of the form ...
-    var x = document.querySelectorAll('.tab')[n]
-    x.style.display = "block";
-    // ... and fix the Previous/Next buttons:
-    if (n == 0) {
-        document.getElementById("previous").style.display = "none";
-    } else {
-        document.getElementById("previous").style.display = "inline";
-    }
-    if (n == (x.length - 1)) {
-        document.getElementById("next").innerHTML = "Submit";
-    } else {
-        document.getElementById("next").innerHTML = "Next";
-    }
-    }
+    var current_fs, next_fs, previous_fs; //fieldsets
+  
     
-    let next = document.getElementById('next')
-    next.addEventListener('click', function (){
-        nextPrev(1)
+    $(".next").click(function(){
+    
+    current_fs = $(this).parent();
+    next_fs = $(this).parent().next();
+    
+    //Add Class Active
+    $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+    
+    //show the next fieldset
+    next_fs.show();
+    });
+    
+    $(".previous").click(function(){
+    
+    current_fs = $(this).parent();
+    previous_fs = $(this).parent().prev();
+    
+    //Remove class active
+    $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
+    
+    //show the previous fieldset
+    previous_fs.show();
+    
+    });
+    
+    $('.radio-group .radio').click(function(){
+    $(this).parent().find('.radio').removeClass('selected');
+    $(this).addClass('selected');
+    });
+    
+    $(".submit").click(function(){
+    return false;
     })
-    let prev = document.getElementById('previous')
-    prev.addEventListener('click', function (){
-        nextPrev(-1)
-    })
-}
-
-
     
+    });
